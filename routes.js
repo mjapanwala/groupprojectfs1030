91fdb286bb3d4ec29a9e17023b51c43472f75249
vadim.lidich@gmail.com <vadim.lidich@gmail.com>
2020-04-25 04:29:08 -0400

    Final changes to routes (allergies + billing)

--- routes.js
+++ routes.js
@@ -24,20 +24,6 @@ app.post('/patients', (req, res) => {
   })
 })
 
-app.get('/patients/:medical_id', (req, res) => {
-  connection.query(`Select * from Patients WHERE medical_id="${req.params.medical_id}"`, (err, result) => {
-    if (err){
-      console.log(err);
-      return res.status(400).send('Bad Request')
-    }
-    if (result.length > 0){
-      console.log(`Returned ${result.length} entries from the DB`);
-      return res.status(200).send(result[0])
-    }
-    console.log('No records found')
-    return res.status(404).send('not found')
-  })
-})
 
 app.put('/patients/:medical_id', (req, res) => {
   connection.query(`Update Patients SET first_name='${req.body.first_name}', last_name='${req.body.last_name}', email="${req.body.email}", gender="${req.body.gender}", dob="${req.body.dob}", phone="${req.body.phone}", mpn="${req.body.mpn}", age="${req.body.age}", address="${req.body.address}" WHERE medical_id="${req.params.medical_id}"`, (err, result) => {
@@ -61,9 +47,20 @@ app.delete('/patients/:medical_id', (req, res) => {
   })
 })
 
-// Post new app.post('/')
-// Edit existing app.put('/:id')
-// Delete existing app.delete('/:id')
+app.get('/patients/:medical_id', (req, res) => {
+  connection.query(`Select * from Patients WHERE medical_id="${req.params.medical_id}"`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    if (result.length > 0){
+      console.log(`Returned ${result.length} entries from the DB`);
+      return res.status(200).send(result[0])
+    }
+    console.log('No records found')
+    return res.status(404).send('not found')
+  })
+})
 
 app.post('/auth/login', (req, res) => {
   console.log(req.body)
@@ -108,6 +105,40 @@ app.get('/allergies/:medical_id', (req, res) => {
   })
 })
 
+app.post('/allergies', (req, res) => {
+  connection.query(`INSERT INTO Allergies (dietary_restrictions, allergies, food_substitution, food_equipment, patient_medical_id) VALUES ('${req.body.dietary_restrictions}', '${req.body.allergies}', '${req.body.food_substitution}', '${req.body.food_equipment}', '${req.body.patient_medical_id}')`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully created an Allergy record for patient with Patient ID: ${req.body.patient_medical_id}`);
+    return res.status(200).send('success')
+  })
+})
+
+
+app.put('/allergies/:medical_id', (req, res) => {
+  connection.query(`Update Allergies SET dietary_restrictions='${req.body.dietary_restrictions}', allergies='${req.body.allergies}', food_substitution="${req.body.food_substitution}", food_equipment="${req.body.food_equipment}" WHERE patient_medical_id="${req.params.medical_id}"`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully updated an Allergy record for patient with Patient ID: ${req.params.medical_id}`);
+    return res.status(200).send('success')
+  })
+})
+
+app.delete('/allergies/:medical_id', (req, res) => {
+  connection.query(`DELETE FROM Allergies WHERE patient_medical_id="${req.params.medical_id}"`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully deleted an Allergy record with Patient ID: ${req.params.medical_id}`);
+    return res.status(200).send('success')
+  })
+})
+
 app.get('/billing', (req, res) => {
   connection.query('Select * from Billing', (err, result) => {
     if (err){
@@ -134,79 +165,38 @@ app.get('/billing/:medical_id', (req, res) => {
   })
 })
 
-// app.post('/inquiries', (req, res) => {
-//
-//     if (req.body){
-//         connection.query(`INSERT INTO Inquiries (First_Name, Last_Name, Email, Message) VALUES ('${req.body.firstName}', '${req.body.lastName}', '${req.body.email}', "${req.body.message}")`, function (error, results, fields) {
-//           if (error){
-//             console.log(error);
-//             return res.status(400).send('Bad Request')
-//           }
-//           console.log('Successfully inserted new record into Inquiry table');
-//           return res.status(200).send('success')
-//         });
-//     } else {
-//         res.status(400).send('Bad Request')
-//     }
-// })
-//
-// app.put('/inquiries', (req, res) => {
-//
-//     if (req.body){
-//         connection.query(`UPDATE Inquiries SET Status='Read' Where id=${req.body.message_id}`, (err) => {
-//           if (err){
-//             console.log(err);
-//             return res.status(400).send('Bad Request')
-//           }
-//           console.log(`Successfully updated a record with an ID ${req.body.message_id}`);
-//           return res.status(200).send('success')
-//         });
-//     } else {
-//         res.status(400).send('Bad Request')
-//     }
-// })
-//
-// app.delete('/inquiries', (req, res) => {
-//
-//     if (req.body){
-//         connection.query(`DELETE FROM Inquiries Where id=${req.body.message_id}`, (err) => {
-//           if (err){
-//             console.log(err);
-//             return res.status(400).send('Bad Request')
-//           }
-//           console.log(`Successfully deleted a record with an ID ${req.body.message_id}`);
-//           return res.status(200).send('success')
-//         });
-//     } else {
-//         res.status(400).send('Bad Request')
-//     }
-// })
-//
-// app.get('/skills', (req, res) => {
-//   connection.query('Select * from Skills Where id=1', (err, result) => {
-//     if (err){
-//       console.log(err);
-//       return res.status(400).send('Bad Request')
-//     }
-//     console.log(`Returned ${result.length} entries from the DB`);
-//     return res.status(200).send(result[0])
-//   })
-// })
-//
-// app.put('/skills', (req, res) => {
-//
-//     if (req.body){
-//         connection.query(`UPDATE Skills SET html=${req.body.html}, css=${req.body.css}, js=${req.body.js} Where id=1`, (err) => {
-//           if (err){
-//             console.log(err);
-//             return res.status(400).send('Bad Request')
-//           }
-//           console.log(`Successfully updated default skills record`);
-//           return res.status(200).send('success')
-//         });
-//     } else {
-//         res.status(400).send('Bad Request')
-//     }
-// })
+app.post('/billing', (req, res) => {
+  connection.query(`INSERT INTO Billing (full_name, email, address, city, province, postal_code, card_name, card_number, exp_month, exp_year, cvv, patient_medical_id) VALUES ('${req.body.full_name}', '${req.body.email}', '${req.body.address}', '${req.body.city}', '${req.body.province}', '${req.body.postal_code}', '${req.body.card_name}', ${req.body.card_number}, '${req.body.exp_month}', ${req.body.exp_year}, ${req.body.cvv}, '${req.body.patient_medical_id}')`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully created a Billing record for patient with Patient ID: ${req.body.patient_medical_id}`);
+    return res.status(200).send('success')
+  })
+})
+
+
+app.put('/billing/:medical_id', (req, res) => {
+  connection.query(`Update Billing SET full_name='${req.body.full_name}', email='${req.body.email}', address="${req.body.address}", city="${req.body.city}", province="${req.body.province}", postal_code="${req.body.postal_code}", card_name="${req.body.card_name}", card_number=${req.body.card_number}, exp_month="${req.body.exp_month}", exp_year=${req.body.exp_year}, cvv=${req.body.cvv} WHERE patient_medical_id="${req.params.medical_id}"`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully updated a Billing record for patient with Patient ID: ${req.params.medical_id}`);
+    return res.status(200).send('success')
+  })
+})
+
+app.delete('/billing/:medical_id', (req, res) => {
+  connection.query(`DELETE FROM Billing WHERE patient_medical_id="${req.params.medical_id}"`, (err, result) => {
+    if (err){
+      console.log(err);
+      return res.status(400).send('Bad Request')
+    }
+    console.log(`Successfully deleted a Billing record with Patient ID: ${req.params.medical_id}`);
+    return res.status(200).send('success')
+  })
+})
 
 module.exports = app;
